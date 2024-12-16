7592a9bd508c90b7019cea091978119a2abe2537
Phuong <114877851+Reinhea@users.noreply.github.com>
2024-12-16 14:44:41 +0100

    Update Main.java

--- gomoku/src/main/Main.java
+++ gomoku/src/main/Main.java
@@ -6,36 +6,21 @@ import jouer.Jouer;
 public class Main {
     public static void main(String[] args) {
         Plateau plateau = new Plateau();
-        plateau.afficherPlateau();
-
 
         Jouer.play("play black C3", plateau);
-        plateau.afficherPlateau();
 
 
         Jouer.play("play white D4", plateau);
         plateau.afficherPlateau();
-
-
-        Jouer.play("play black C3", plateau);
-        plateau.afficherPlateau();
-
+        
+        Jouer.play("play white C3", plateau);
 
         Jouer.play("play black A1", plateau);
         plateau.afficherPlateau();
-        
+
         plateau.clearPlateau();
-
-        Jouer.play("play black A9", plateau);
+        System.out.println();
         plateau.afficherPlateau();
-
-        Jouer.play("play red A5", plateau);
-        plateau.afficherPlateau();
-
-        Jouer.play("play black CC7", plateau);
-        plateau.afficherPlateau();
-
-        plateau.quit();
-
+        
     }
 }
